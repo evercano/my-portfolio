c5b8c7c516a9fee0d1b257148110a8298d42e698
evercano <evercano.tech@gmail.com>
2025-06-05 20:58:31 -0600

    Add domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,8 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  vite: {
+    site: "evercano.github.io.",
+    vite: {
       plugins: [tailwindcss()],
   },
 
